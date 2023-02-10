d419fa8cee4564eeec22db5dc21f0a33e2d654fb
douglashigoshi <douglas.santos@me.com.br>
2023-02-10 17:48:59 -0300

    Fixing BDD in base project

--- cypress/e2e/normal-login.cy.js
+++ cypress/e2e/normal-login.cy.js
@@ -1,37 +1,51 @@
 /// <reference types="cypress" />
 
-describe('Após acessar o site demoblaze', () => {
+describe("Após acessar o site demoblaze", () => {
+
+  const user_num = Math.floor(Math.random() * 1000000);
+  const user = `ME_user_${user_num}`;
+  const password = "me_password";
 
   beforeEach(() => {
-    cy.visit('https://www.demoblaze.com/')
-  })
+    cy.visit("https://www.demoblaze.com/");
+  });
 
-  it('Devemos criar um usuário', () => {
+  it("Dado que criamos um usuário", () => {
+    cy.get("#signin2").click();
 
-    cy.get('#signin2').click()
+    cy.wait(500);
+    cy.get("#sign-username").type(user);
+    cy.get("#sign-password").type(password);
 
-    cy.wait(500)
-    cy.get('#sign-username').type('ME_user_01')
-    cy.get('#sign-password').type('ME_password')
+    cy.get('button[onclick="register()"]').click();
 
-    cy.get('button[onclick="register()"]').click()
+    cy.on("window:confirm", (text) => {
+      expect(text).to.equal("Sign up successful.");
+    });
 
-    cy.on('window:confirm', text => {
-      expect(text).to.equal('Sign up successful.')
-    })
-  })
+    cy.wait(1000);
+  });
 
-  it('Para realizar login com esse usuário', () => {
+  it("Quando realizar login com esse usuário", () => {
+    cy.get("#login2").click();
 
-    cy.get('#login2').click()
+    cy.wait(500);
+    cy.get("#loginusername").type(user);
+    cy.get("#loginpassword").type(password);
 
-    cy.wait(500)
-    cy.get('#loginusername').type('ME_user_01')
-    cy.get('#loginpassword').type('ME_password')
+    cy.get('button[onclick="logIn()"]').click();
+  });
 
-    cy.get('button[onclick="logIn()"]').click()
+  it("Então devo ver usuário no topo da página", () => {
+    cy.get("#login2").click();
 
-    cy.wait(1000)
-    cy.get('#nameofuser').should('have.text', 'Welcome ME_user_01')
-  })
-})
+    cy.wait(500);
+    cy.get("#loginusername").type(user);
+    cy.get("#loginpassword").type(password);
+
+    cy.get('button[onclick="logIn()"]').click();
+
+    cy.wait(1000);
+    cy.get("#nameofuser").should("have.text", `Welcome ${user}`);
+  });
+});
