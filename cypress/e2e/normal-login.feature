0ffbd02b5e93ff24fc577df79552c32d4337d5fc
douglashigoshi <douglas.santos@me.com.br>
2023-02-13 17:24:26 -0300

    Adding step parameter and mock data

--- cypress/e2e/normal-login.feature
+++ cypress/e2e/normal-login.feature
@@ -5,3 +5,8 @@ Funcionalidade: Criação de usuário
     Dado que criamos um usuário
     Quando realizar login com esse usuário
     Então devo ver usuário no topo da página
+
+  Cenário: Logando com um usuário existente
+    Dado que o usuário "usuario_ME" já está cadastrado
+    Quando tentar criar o usuário "usuario_ME"
+    Então devo validar o alerta "signupError"
--- cypress/e2e/normal-login.js
+++ cypress/e2e/normal-login.js
@@ -1,4 +1,6 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+import * as validation from "../fixtures/example.json"
+import * as user from "../fixtures/user.json"
 
 const user_num = Math.floor(Math.random() * 1000000)
 const user = `ME_user_${user_num}`;
@@ -34,4 +36,37 @@ When("realizar login com esse usuário", () => {
 Then("devo ver usuário no topo da página", () => {
   cy.wait(1000);
   cy.get("#nameofuser").should("have.text", `Welcome ${user}`);
+})
+
+Given("que o usuário {string} já está cadastrado", (user) => {
+  cy.visit('https://Demoblaze.com')
+  cy.get("#login2").click();
+
+  cy.wait(500);
+  cy.get("#loginusername").type(user.me);
+  cy.get("#loginpassword").type(user.password);
+
+  cy.get('button[onclick="logIn()"]').click();
+
+  cy.wait(1000);
+  cy.get("#nameofuser").should("have.text", `Welcome ${user}`);
+
+  cy.get('#logout2').click()
+
+})
+
+When('tentar criar o usuário {string}', (user) => {
+  cy.get("#signin2").click();
+
+  cy.wait(500);
+  cy.get("#sign-username").type(user);
+  cy.get("#sign-password").type(user);
+
+  cy.get('button[onclick="register()"]').click();
+})
+
+Then("devo validar o alerta {string}", (alert) => {
+  cy.on("window:confirm", (text) => {
+    expect(text).to.equal(validation[alert]);
+  });
 })
